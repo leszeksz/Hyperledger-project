9cb11704a86f14f296a34dfe46cbb0b0ebd65508
Szczotka <leszek.szczotka@accenture.com>
2022-04-01 14:33:18 +0200

    exceptions handle

--- asset-transfer-basic/application-gateway-java/src/main/java/App.java
+++ asset-transfer-basic/application-gateway-java/src/main/java/App.java
@@ -71,22 +71,12 @@ public final class App {
 		// this endpoint.
 		ManagedChannel channel = newGrpcConnection();
 
-		Gateway.Builder builder = Gateway.newInstance().identity(newIdentity()).signer(newSigner()).connection(channel)
+		Gateway.newInstance().identity(newIdentity()).signer(newSigner()).connection(channel)
 				// Default timeouts for different gRPC calls
 				.evaluateOptions(CallOption.deadlineAfter(5, TimeUnit.SECONDS))
 				.endorseOptions(CallOption.deadlineAfter(15, TimeUnit.SECONDS))
 				.submitOptions(CallOption.deadlineAfter(5, TimeUnit.SECONDS))
 				.commitStatusOptions(CallOption.deadlineAfter(1, TimeUnit.MINUTES));
-
-//		try (Gateway gateway = builder.connect()) {
-//			new App(gateway).run();
-//		}
-//		catch (Exception e){
-//			System.out.println(e.getStackTrace());
-//		}
-//			finally {
-//			channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
-//		}
 	}
 
 	private static ManagedChannel newGrpcConnection() throws IOException, CertificateException {
@@ -123,138 +113,4 @@ public final class App {
 		// Get the smart contract from the network.
 		contract = network.getContract(chaincodeName);
 	}
-
-//	public void run() throws GatewayException, CommitException {
-////		 Initialize a set of asset data on the ledger using the chaincode 'InitLedger' function.
-////		initLedger();
-//
-////		// Return all the current assets on the ledger.
-////		getAllAssets();
-////
-////		// Create a new asset on the ledger.
-////		createAsset();
-////
-////		// Update an existing asset asynchronously.
-////		transferAssetAsync();
-////
-////		// Get the asset details by assetID.
-////		readAssetById();
-////
-////		// Update an asset which does not exist.
-////		updateNonExistentAsset();
-//	}
-//
-//	/**
-//	 * This type of transaction would typically only be run once by an application
-//	 * the first time it was started after its initial deployment. A new version of
-//	 * the chaincode deployed later would likely not need to run an "init" function.
-//	 */
-//	private void initLedger() throws EndorseException, SubmitException, CommitStatusException, CommitException {
-//		System.out.println("\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger");
-//
-//		contract.submitTransaction("InitLedger");
-//
-//		System.out.println("*** Transaction committed successfully");
-//	}
-//
-//	/**
-//	 * Evaluate a transaction to query ledger state.
-//	 */
-//	private void getAllAssets() throws GatewayException {
-//		System.out.println("\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger");
-//
-//		byte[] result = contract.evaluateTransaction("GetAllAssets");
-//
-//		System.out.println("*** Result: " + prettyJson(result));
-//	}
-//
-//	private String prettyJson(final byte[] json) {
-//		return prettyJson(new String(json, StandardCharsets.UTF_8));
-//	}
-//
-//	private String prettyJson(final String json) {
-//		JsonElement parsedJson = JsonParser.parseString(json);
-//		return gson.toJson(parsedJson);
-//	}
-//
-//	/**
-//	 * Submit a transaction synchronously, blocking until it has been committed to
-//	 * the ledger.
-//	 */
-//	private void createAsset() throws EndorseException, SubmitException, CommitStatusException, CommitException {
-//		System.out.println("\n--> Submit Transaction: CreateAsset, creates new asset with ID, Color, Size, Owner and AppraisedValue arguments");
-//
-//		contract.submitTransaction("CreateAsset", assetId, "yellow", "5", "Tom", "1300");
-//
-//		System.out.println("*** Transaction committed successfully");
-//	}
-//
-//	/**
-//	 * Submit transaction asynchronously, allowing the application to process the
-//	 * smart contract response (e.g. update a UI) while waiting for the commit
-//	 * notification.
-//	 */
-//	private void transferAssetAsync() throws EndorseException, SubmitException, CommitStatusException {
-//		System.out.println("\n--> Async Submit Transaction: TransferAsset, updates existing asset owner");
-//
-//		SubmittedTransaction commit = contract.newProposal("TransferAsset")
-//				.addArguments(assetId, "Saptha")
-//				.build()
-//				.endorse()
-//				.submitAsync();
-//
-//		byte[] result = commit.getResult();
-//		String oldOwner = new String(result, StandardCharsets.UTF_8);
-//
-//		System.out.println("*** Successfully submitted transaction to transfer ownership from " + oldOwner + " to Saptha");
-//		System.out.println("*** Waiting for transaction commit");
-//
-//		Status status = commit.getStatus();
-//		if (!status.isSuccessful()) {
-//			throw new RuntimeException("Transaction " + status.getTransactionId() +
-//					" failed to commit with status code " + status.getCode());
-//		}
-//
-//		System.out.println("*** Transaction committed successfully");
-//	}
-//
-//	private void readAssetById() throws GatewayException {
-//		System.out.println("\n--> Evaluate Transaction: ReadAsset, function returns asset attributes");
-//
-//		byte[] evaluateResult = contract.evaluateTransaction("ReadAsset", assetId);
-//
-//		System.out.println("*** Result:" + prettyJson(evaluateResult));
-//	}
-//
-//	/**
-//	 * submitTransaction() will throw an error containing details of any error
-//	 * responses from the smart contract.
-//	 */
-//	private void updateNonExistentAsset() {
-//		try {
-//			System.out.println("\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error");
-//
-//			contract.submitTransaction("UpdateAsset", "asset70", "blue", "5", "Tomoko", "300");
-//
-//			System.out.println("******** FAILED to return an error");
-//		} catch (EndorseException | SubmitException | CommitStatusException e) {
-//			System.out.println("*** Successfully caught the error: ");
-//			e.printStackTrace(System.out);
-//			System.out.println("Transaction ID: " + e.getTransactionId());
-//
-//			List<ErrorDetail> details = e.getDetails();
-//			if (!details.isEmpty()) {
-//				System.out.println("Error Details:");
-//				for (ErrorDetail detail : details) {
-//					System.out.println("- address: " + detail.getAddress() + ", mspId: " + detail.getMspId()
-//							+ ", message: " + detail.getMessage());
-//				}
-//			}
-//		} catch (CommitException e) {
-//			System.out.println("*** Successfully caught the error: " + e);
-//			e.printStackTrace(System.out);
-//			System.out.println("Transaction ID: " + e.getTransactionId());
-//			System.out.println("Status code: " + e.getCode());
-//		}
-//	}
 }
--- asset-transfer-basic/application-java/src/main/java/application/java/App.java
+++ asset-transfer-basic/application-java/src/main/java/application/java/App.java
@@ -77,21 +77,16 @@ public class App {
 		while (userChoice != DEFAULT_USER_CHOICE) {
 			switch (userChoice) {
 				case 1:
-					contract.submitTransaction("CreateAsset", "asset14", "Tom", "1300");
-					System.out.println("Submit Transaction: CreateAsset asset13");
+					createAsset(contract, scanner);
 					break;
 				case 2:
-					result = contract.evaluateTransaction("GetAllAssets");
-					System.out.println("Evaluate Transaction: GetAllAssets, result: " + new String(result));
+					getAssets(contract);
 					break;
 				case 3:
-					result = contract.evaluateTransaction("ReadAsset", "asset13");
-					System.out.println("result: " + new String(result));
+					readAsset(contract, scanner);
 					break;
 				case 4:
-					contract.submitTransaction("UpdateAsset", "asset1", "Tomoko", "350");
-					System.out.println("Asset updated");
-					break;
+					updateAsset(contract, scanner);
 			}
 			userChoice = getUserInput(scanner);
 		}
@@ -99,7 +94,61 @@ public class App {
 		System.out.println("Bye");
 	}
 
+	private static void updateAsset(Contract contract, Scanner scanner) throws TimeoutException, InterruptedException {
+		System.out.println("Enter the product id you want to change:");
+		String productIdToChange = scanner.next();
+		System.out.println("Enter new owner:");
+		String ownerToChange = scanner.next();
+		System.out.println("Enter new price:");
+		String priceToChange = scanner.next();
+		if (!validatePrice(priceToChange)){
+			System.out.println("Price must be greater than zero. Enter price again:");
+			priceToChange = scanner.next();
+		}
+		try {
+			contract.submitTransaction("UpdateAsset", productIdToChange, ownerToChange, priceToChange);
+			System.out.println("Asset updated");
+		}catch (ContractException exception){
+			System.out.println("Invalid product Id");
+		}
+	}
 
+	private static void readAsset(Contract contract, Scanner scanner) {
+		byte[] result;
+		System.out.println("Enter the product id you want to check:");
+		String productName = scanner.next();
+		try {
+			result = contract.evaluateTransaction("ReadAsset", productName);
+			System.out.println("result: " + new String(result));
+		}catch (ContractException exception){
+			System.out.println("Can't find asset with id: " + productName);
+		}
+	}
+
+	private static void getAssets(Contract contract) throws ContractException {
+		byte[] result;
+		result = contract.evaluateTransaction("GetAllAssets");
+		System.out.println("Evaluate Transaction: GetAllAssets, result: " + new String(result));
+	}
+
+	private static void createAsset(Contract contract, Scanner scanner) throws TimeoutException, InterruptedException {
+		System.out.println("Enter the product id:");
+		String productId = scanner.next();
+		System.out.println("Enter owner:");
+		String owner = scanner.next();
+		System.out.println("Enter price:");
+		String price = scanner.next();
+		if (!validatePrice(price)){
+			System.out.println("Price must be greater than zero. Enter price again:");
+			price = scanner.next();
+		}
+		try {
+			contract.submitTransaction("CreateAsset", productId, owner, price);
+			System.out.println("Submit Transaction: CreateAsset " + productId);
+		}catch (ContractException exception){
+			System.out.println("Something went wrong. Try again.");
+		}
+	}
 
 	private static int getUserInput(Scanner scanner){
 
@@ -118,7 +167,12 @@ public class App {
 			System.out.println("Scanner is closed");
 			e.printStackTrace();
 		}
-
 		return userChoice;
+	}
+	private static boolean validatePrice(String price){
+		int number = Integer.parseInt(price);
+		if (number < 0){
+			return false;
+		}else return true;
 	}
 }
